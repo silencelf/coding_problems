c42a790a4da43b68636c53cfdde8e68a762e4ae2
= <=>
2019-04-08 00:41:03 +0800

    initial commit

---
+++ number_sum.py
@@ -0,0 +1,46 @@
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        ret = func(*args, **kwargs)
+        end = time.time()
+        print("--- %.5f seconds ---" % (end - start))
+        return ret
+    return wrapper
+
+def tests():
+    input = [10, 15, 3, 7]
+    k = 17
+    result = check_sum(input, k)
+    print('%d >> %s' % (k, result))
+    result = check_sum2(input, k)
+    print('%d >> %s' % (k, result))
+
+    input = [0 for _ in range(0, 10000)]
+    k = 10005
+    result = check_sum(input, k)
+    print('%d >> %s' % (k, result))
+    result = check_sum2(input, k)
+    print('%d >> %s' % (k, result))
+
+@timer
+def check_sum(input, k):
+    for i in range(0, len(input)):
+        for j in range(i + 1, len(input)):
+            if input[i] + input[j] == k:
+                return True
+    return False
+
+@timer
+def check_sum2(input, k):
+    cache = {}
+    for i in input:
+        if i in cache:
+            return True
+        else:
+            cache[k - i] = i
+    return False
+
+# run tests
+tests()
---
+++ stair_case.py
@@ -0,0 +1,116 @@
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        ret = func(*args, **kwargs)
+        end = time.time()
+        print("--- %s seconds ---" % (end - start))
+        return ret
+    return wrapper
+
+def test_staircase():
+    suits = [1, 2, 3, 4 ,5 , 11, 30]
+    for i in suits:
+        print(staircase(i))
+        print(staircase_iter(i))
+        print(staircase_multi(i, [1, 2]))
+
+def staircase_iter(n):
+    a, b = 1, 2
+    for _ in range(1, n):
+        a, b = b, a + b
+    return a
+
+def staircase(n):
+    if n <= 1:
+        return 1
+    else:
+        return staircase(n - 1) + staircase(n - 2)
+
+def staircase_dp(n, dict):
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+    if n in dict:
+        return dict[n]
+    ret = staircase_dp(n - 1, dict) + staircase_dp(n - 2, dict)
+    dict[n] = ret
+    return ret
+
+def staircase_multi(n, X):
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        return sum(staircase_multi(n - x, X) for x in X if n - x >= 0)
+
+# usally i would stop with this as it is readable and it is fast
+def staircase_multi_dp(n, X, cache):
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+
+    if n in cache:
+        return cache[n]
+
+    cache[n] = sum(staircase_multi_dp(n - x, X, cache) for x in X if n - x >= 0)
+    return cache[n]
+
+# but for pactice let's try the iterative way
+def staircase_multi_iter(n, X):
+    cache = [0 for _ in range(n + 1)]
+    cache[0] = 1
+    for i in range(1, n + 1):
+        cache[i] += sum(cache[i - x] for x in X if n - x >= 0)
+
+    return cache[n]
+
+# this is where the tests begin
+count = 35
+steps = [2, 3]
+
+# brutal force
+start = time.time()
+print(staircase(count))
+end = time.time()
+print("simple: --- %.4f seconds ---" % (end - start))
+
+# dynamic programming
+start = time.time()
+dict = {}
+print(staircase_dp(count, dict))
+end = time.time()
+print(dict)
+print("simple DP: --- %.4f seconds ---" % (end - start))
+
+# iterative
+start = time.time()
+print(staircase_iter(count))
+end = time.time()
+print("simple iterator: --- %.4f seconds ---" % (end - start))
+
+# flexable but not efficient
+start = time.time()
+print(staircase_multi(count, steps))
+end = time.time()
+print("flexible: --- %.4f seconds ---" % (end - start))
+
+# flexable but not efficient
+start = time.time()
+cache = {}
+print(staircase_multi_dp(count, steps, cache))
+end = time.time()
+print(cache)
+print("flexible DP: --- %.4f seconds ---" % (end - start))
+
+# flexable iteritave with dp
+start = time.time()
+print(staircase_multi_iter(count, steps))
+end = time.time()
+print("flexible iter DP: --- %.4f seconds ---" % (end - start))
+
+#test_staircase()
