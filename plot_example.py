28e0fcf45e7bbcb3550877023b205d0eabafa1c7
= <=>
2019-04-08 00:52:40 +0800

    add example for plot

---
+++ plot_example.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.random.randn(60)
+y = np.random.randn(60)
+
+plt.scatter(x, y, s=20)
+
+out_png = 'path/to/store/out_file.png'
+plt.savefig(out_png, dpi=150)
