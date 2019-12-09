b522092286165b2cae9d188372b20377e9deb517
Kou <kent.kou@activenetwork.com>
2019-12-09 18:02:02 +0800

    add tree traversal methods

---
+++ traversal.py
@@ -0,0 +1,51 @@
+#! /usr/local/bin/python33
+
+class Node:
+  def __init__(self, val, left = None, right = None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+root = Node('F')
+root.left = Node('B', Node('A'), Node('D', Node('C'), Node('E')))
+root.right = Node('G', None, Node('I', Node('H')))
+
+def pre_order_traversal(node):
+  if not node:
+    return []
+
+  result = []
+  result.append(node.val)
+  result += pre_order_traversal(node.left)
+  result += pre_order_traversal(node.right)
+
+  return result
+
+print('pre order traversal')
+arr = pre_order_traversal(root)
+print(arr)
+
+def level_order_traversal(node):
+  result = []
+  if not node:
+    return
+
+  queue = [node]
+  while len(queue):
+    level = []
+    queue2 = []
+    for item in queue:
+      level.append(item.val)
+      if item.left:
+        queue2.append(item.left)
+      if item.right:
+        queue2.append(item.right)
+
+    result.append(level)
+    queue = queue2
+  return result
+
+
+print('level order traversal')
+arr = level_order_traversal(root)
+print(arr)
