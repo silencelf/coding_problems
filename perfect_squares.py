c936cc7933dff0e3f3a7f613f71032390e8db084
Kou <kent.kou@activenetwork.com>
2019-12-18 15:09:29 +0800

    performance improvement

--- perfect_squares.py
+++ perfect_squares.py
@@ -2,24 +2,24 @@ from collections import deque
 
 class Solution:
     def numSquares(self, n: int) -> int:
-        step, queue, visited = -1, deque([0]), set()
-        numbers = [i*i for i in range(1, n + 1) if i*i <= n]
+        step, queue, visited, nq = -1, [0], set(), []
+        numbers = [i**2 for i in range(1, int(n**0.5) + 1)]
 
         while queue:
-            l = len(queue)
             step += 1
-            for _ in range(l):
-                num = queue.popleft()
+            for num in queue:
                 if num == n:
                     return step
                 if num in visited:
                     continue
                 visited.add(num)
                 for i in numbers:
-                    queue.append(num + i)
-
+                    newVal = num + i
+                    if newVal > n:
+                        break;
+                    nq.append(newVal)
+            queue, nq = nq, []
         return -1
-
 
 solution = Solution()
 input = 12
@@ -33,6 +33,11 @@ result = solution.numSquares(input)
 print(f'expected {expected}, actual result is {result}')
 
 input = 9
+expected = 1
+result = solution.numSquares(input)
+print(f'expected {expected}, actual result is {result}')
+
+input = 1
 expected = 1
 result = solution.numSquares(input)
 print(f'expected {expected}, actual result is {result}')
