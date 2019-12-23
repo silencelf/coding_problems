f6bf241c4e8a46c153455cdf7a27d47da8d335f8
Kou <kkou@xia34543m.active.local>
2019-12-23 17:41:26 +0800

    first but wrong code.

--- findTargetSumWays.py
+++ findTargetSumWays.py
@@ -1,7 +1,22 @@
 #! /usr/bin/python3
 
+from collections import deque
+
 def findTargetSumWays(nums, s):
-    return -1
+    nu = deque(nums)
+    def search(numbers, sign, current, target):
+        if not numbers:
+            return 1 if current == target else 0
+
+        value = numbers.popleft()
+        if sign == '+':
+            current += value
+        else:
+            current -= value
+        plus = search(numbers.copy(), '+', current, target) 
+        minus = search(numbers.copy(), '-', current, target) 
+        return plus + minus
+    return search(nu.copy(), '+', 0, s) + search(nu.copy(), '-', 0, s)
 
 nums, s = [1,1,1,1,1], 3
 expected = 5
