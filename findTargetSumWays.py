e1ca739819ae7300b7694922bd95241ec1eec6d3
Kou <kkou@xia34543m.active.local>
2019-12-23 15:24:38 +0800

    initial commit

---
+++ findTargetSumWays.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+def findTargetSumWays(nums, s):
+    return -1
+
+nums, s = [1,1,1,1,1], 3
+expected = 5
+actual = findTargetSumWays(nums, s)
+print(f"expected {expected}, actual: {actual}")
