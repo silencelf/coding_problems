baca0cb849c90d61c72ac545c44499b7a458901d
kkou <a@b.c>
2020-01-10 15:03:09 +0800

    add nqueen solution

---
+++ nqueen.py
@@ -0,0 +1,10 @@
+#! /usr/local/bin/python3
+class Solution:
+    def totalQueens(self, n):
+        return 0
+
+
+n = 4
+s = Solution()
+result = s.totalQueens(n)
+print(f'n = {n},  solutions: {result}')
