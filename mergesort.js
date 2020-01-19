8b762acb088af8e2dc8026b39539d36af46696a4
kkou <a@b.c>
2020-01-19 13:51:11 +0800

    adding merge sort by javascript

---
+++ mergesort.js
@@ -0,0 +1,48 @@
+function mergeSort(input) {
+  if (input.length <= 1) return input;
+  const index = parseInt(input.length/2);
+  let right = input.splice(index);
+  const la = mergeSort(input);
+  const ra = mergeSort(right);
+  let newArr = merge(la, ra);
+
+  return newArr;
+}
+
+function merge(left, right) {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  const result = [];
+
+  while(leftIndex < left.length && rightIndex < right.length) {
+    const le = left[leftIndex];
+    const re = right[rightIndex];
+    if (le <= re) {
+      result.push(le);
+      leftIndex++;
+    } else {
+      result.push(re);
+      rightIndex++;
+    }
+  }
+
+  while (leftIndex < left.length) {
+      result.push(left[leftIndex++]);
+  }
+
+  while (rightIndex < right.length) {
+      result.push(right[rightIndex++]);
+  }
+
+  return result;
+}
+
+let input = [3, 1, 23, 2, 235, 88, 634, 27, 73, 56, 89, 14, 22];
+let sorted = mergeSort(input);
+console.log('sorted array:')
+console.log(sorted);
+
+input = [1, 2, 3, 4 , 5];
+sorted = mergeSort(input);
+console.log('sorted array:')
+console.log(sorted);
