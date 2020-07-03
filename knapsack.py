c75ecf37b9f5cd70a1c39d12a71d05d57006ea3f
kkou <a@b.c>
2020-07-03 21:28:14 +0800

    knapsack problem

--- knapsack.py
+++ knapsack.py
@@ -1,4 +1,23 @@
-#! /usr/python3
+#! /usr/bin/python3
 
-def ns(n, s)
-    print(1)
+values = [60, 100, 120, 130]
+weights = [10, 20, 30, 30]
+W = 50
+
+"" naive version,O(2^n)
+def knapsack(n, s):
+    print(f'n = {n}, s = {s}')
+    w = weights[n]
+    v = values[n]
+    print(f'w = {w}, v = {v}')
+    if n == -1 or s == 0:
+        return 0
+    if w > s:
+        return knapsack(n - 1, s)
+    else:
+        v1 = knapsack(n - 1, s)
+        v2 = v + knapsack(n - 1, s - w)
+        return max(v1, v2)
+
+result = knapsack(len(values) - 1, W)
+print(f'knapset result: {result}')
