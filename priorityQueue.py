71c3b8796c29d2bb2b3c8a35c018b500ac6689c7
kkou <a@b.c>
2020-07-26 13:10:33 +0800

    try to use PriorityQueue of py

--- priorityQueue.py
+++ priorityQueue.py
@@ -1,8 +1,14 @@
 #! /user/bin/python3
 
-class PQ:
-    def pull():
-        pass
+from queue import PriorityQueue
 
-    def add(value):
-        pass
+customers = PriorityQueue()
+
+customers.put(2, "Harry")
+customers.put(3, "Charles")
+customers.put(1, "Riya")
+customers.put(4, "Stacy")
+
+while customers:
+    print('-'*20)
+    print(customers.get())
