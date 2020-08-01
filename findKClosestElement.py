dab7b117a39d556b9406c7a232f9e50fa888d1ff
kkou <kkou@a.c>
2020-08-01 23:01:33 +0800

    update code

--- findKClosestElement.py
+++ findKClosestElement.py
@@ -23,21 +23,46 @@ Absolute value of elements in the array and x will not exceed 104
 """
 
 class Solution:
+    def findClosestElements(self, arr, k, x):
+        if len(arr) == 0:
+            return []
+        index = self.binSearch(arr, x)
+        result = self.findClosest(arr, k, x, index)
+        return result
+
     def binSearch(self, arr, x): 
         left, right = 0, len(arr) - 1
         while left <= right:
             mid = (left + right) // 2
             if arr[mid] == x:
-                return (True, mid)
+                return mid
             elif arr[mid] < x:
                 left = mid + 1
             else:
                 right = mid - 1
-        return (False, mid)
+        return mid
 
-    def findClosestElements(self, arr, k, x):
-        index = self.binSearch(arr, x)
-        return index
+    def findClosest(self, arr, k, x, index):
+        result = []
+        if arr[index] == x:
+            result.append(arr[index])
+        i = index - 1
+        j = index + 1
+        while k > 1:
+            left = arr[i] - x
+            right = arr[j] - x
+            print(i)
+            print(j)
+            if i > -1 and left <= right:
+                print('left')
+                result.append(arr[i])
+                i -= 1
+            elif j < len(arr):
+                print('right')
+                result.append(arr[j])
+                j += 1
+            k -= 1
+        return result
 
 s = Solution()
 
@@ -54,4 +79,7 @@ expected = [1,2,3,4]
 #     index += 1
 
 result = s.findClosestElements(arr, k, 3)
+print(result)
+
+result = s.findClosestElements(arr, 3, 4)
 print(result)
