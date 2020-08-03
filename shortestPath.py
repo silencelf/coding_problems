f1512dee843cca8d3efe5243960e0c8d4fbac878
kkou <kkou@a.c>
2020-08-04 01:24:51 +0800

    tweak

--- shortestPath.py
+++ shortestPath.py
@@ -18,13 +18,15 @@ adj[e] = [(4 ,d)]
 adj[f] = []
 
 def shortest_path(adj, s):
-    # have to write a priority queue myself
-    arr = [(0, s)] + [(math.inf, v) for v in adj if v != s]
-    D = { v: math.inf for v in adj }
-    D[s] = 0
-    print(D)
-    P = { v: None for v in adj }
-    print(P)
+    arr, D, P = [], {}, {}
+    for v in adj:
+        P[v] = None
+        if v == s:
+            arr.append((0, v))
+            D[v] = 0
+        else:
+            arr.append((math.inf, v))
+            D[v] = math.inf
     heap = Heap(arr)
 
     while True:
