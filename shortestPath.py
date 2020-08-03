16a27e4b418b028c2870c5ff574ad8ebe0bb1d1d
kkou <kkou@a.c>
2020-08-03 18:36:40 +0800

    init shortest path problem

---
+++ shortestPath.py
@@ -0,0 +1,12 @@
+from vertex import Vertex
+
+v1 = Vertex(1);
+v2 = Vertex(2);
+
+ht = {};
+ht[v1] = 1;
+ht[v2] = 2;
+
+print(ht);
+print(ht[v1]);
+print(ht[v2]);
---
+++ vertex.py
@@ -0,0 +1,9 @@
+#! /bin/python
+
+class Vertex:
+    def __init__(self, val = None):
+        self.val = val
+    
+    def __str__(self):
+        return str(self.val)
+
