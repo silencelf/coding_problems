1f8736901fe1fb3e514d10ba221e622dc4a1d3e8
kkou <a@b.c>
2020-08-14 18:19:22 +0800

    rewrite binary_search using while loop

--- binary_search.py
+++ binary_search.py
@@ -1,16 +1,19 @@
 #! /usr/bin/python
 
-def binary_search(arr, start, end, i):
-    if start > end:
-        return -1
-    mid = (start + end) // 2
-    if arr[mid] == i:
-        return mid
-    elif arr[mid] < i:
-        start = mid + 1
-    else:
-        end = mid - 1
-    return binary_search(arr, start, end, i)
+import random
+
+def binary_search(arr, i):
+    start, end = 0, len(arr) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == i:
+            return mid
+        elif arr[mid] < i:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return -1
 
 # arr = [1, 3, 8, 12 , 15, 16, 44, 56, 70, 100]
 # start, end = 0, len(arr) - 1
@@ -18,6 +21,8 @@ def binary_search(arr, start, end, i):
 # print(index)
 
 arr = [i for i in range(1000000)]
-start, end = 0, len(arr) - 1
-index = binary_search(arr, start, end, 1010000)
-print(index)
+
+for i in range(10):
+    num = random.randrange(2000000)
+    index = binary_search(arr, num)
+    print(f'find {num}, result = {index}')
