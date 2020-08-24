80de487e977ca6bc5747604d4b18873bfc859776
kkou <kkou@a.c>
2020-08-25 00:12:41 +0800

    implement find_integer

--- find_integer.py
+++ find_integer.py
@@ -11,10 +11,19 @@ You can modify the input array in-place.
 """
 
 def find_integer(arr):
+    """
+    i don't like inplace modification so I'll use set anyway
+    """
+    memo = set()
+    for i in arr:
+        memo.add(i)
+    for i in range(1, len(arr) + 1):
+        if i not in memo:
+            return i
     return -1
 
 def test():
-    suits = [ ([3, 4, -1, 1], 2), ([1, 2, 0], 3) ]
+    suits = [ ([3, 4, 4, -1, 1], 2), ([1, 2, 0], 3) ]
     for (param, expected) in suits:
         ret = find_integer(param)
         print(f'expected = {expected}, actual = {ret}')
