daee80967a6e87ca7d235e5c9d812c42dec954b8
kkou <a@b.c>
2020-08-28 12:24:10 +0800

    add description for the problem

--- robot_move.py
+++ robot_move.py
@@ -1,7 +1,10 @@
 #! /bin/python
+"""
+move a robot from (1, 1) to (M, N), the robot can only move right or move up.
+how many distinct path are there for the rebot to move to destination?
+"""
 
-def robot_move():
-    print('...')
+def robot_move(m, n):
+    pass
 
-
-robot_move()
+robot_move(5, 5)
