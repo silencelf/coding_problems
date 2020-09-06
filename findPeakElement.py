7cde48494b72ec5043151a4aeb2e884482c2964a
kkou <kkou@a.c>
2020-09-07 00:44:08 +0800

    heee

--- findPeakElement.py
+++ findPeakElement.py
@@ -26,7 +26,21 @@ class Solution:
         if low == len(nums) -1 and nums[mid] > nums[mid + 1]:
             return low
 
+    def findPeak(self, nums):
+        if not nums:
+            return -1
+        low, high= 0, len(nums) - 1
+        while low < high:
+            mid1 = (high + low) // 2
+            mid2 = mid1 + 1
+            if nums[mid1] < nums[mid2]:
+                low = mid2
+            else:
+                high = mid1
+        return low
+
 sol = Solution()
 nums = [1,2,1,3,5,6,4]
-result  = sol.findPeakElement(nums)
+#result  = sol.findPeakElement(nums)
+result  = sol.findPeak(nums)
 print(f'Peak of {nums} : {nums[result]}')
