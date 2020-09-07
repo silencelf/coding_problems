a9e0813bcd654e1da12968c7bf8795b5089d4326
kkou <a@b.c>
2020-09-07 21:15:38 +0800

    ha

--- job_scheduling.py
+++ job_scheduling.py
@@ -26,10 +26,10 @@ Job = namedtuple('Job', 'start finish val')
 
 def wis(input):
     end_sorted = sorted(input, key=attrgetter('finish'))
+    print('sorted:')
     print(end_sorted)
     # O(n^2) to generate compatible sets
     comp_sets = { j: [i for i in end_sorted if i.start >= j.finish] for j in end_sorted }
-    print(comp_sets)
 
     def dp(jobs):
         if not jobs:
@@ -37,12 +37,27 @@ def wis(input):
         first = jobs[0]
         return max(first.val + dp(comp_sets[first]), dp(jobs[1:]))
 
-#    def dp2(jobs, i):
-#        if not jobs:
-#            return 0
-#        first = jobs[0]
-#        return max(first.val + dp2(comp_sets[first]), dp2(jobs, i + 1))
     return dp(end_sorted)
+#    next_valid = {}
+#    for k in range(len(end_sorted)):
+#        j = k + 1
+#        next_valid[k] = None
+#        while j < len(end_sorted):
+#            if end_sorted[j].start >= end_sorted[k].finish:
+#                next_valid[k] = j
+#                break
+#            j += 1
+#
+#    print('-------------')
+#    print(next_valid)
+#    print('-------------')
+#
+#    def dp2(jobs, i):
+#        if i == None or i >= len(jobs):
+#            return 0
+#        return max(jobs[i].val + dp2(jobs, next_valid[i]), dp2(jobs, i + 1))
+#
+#    return dp2(end_sorted, 0)
 
 
 def tests():
