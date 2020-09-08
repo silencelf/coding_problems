baf7ac15c58f51aece5d9a15edab3611b4f07ef2
kkou <kkou@a.c>
2020-09-08 22:41:15 +0800

    ....

--- kmp.js
+++ kmp.js
@@ -1,23 +1,44 @@
-#!/usr/local/bin/node
+#!/usr/bin/node
 
 class Solution {
   kmp(text, pattern) {
+    const lps = this.buildLPS(pattern);
+
+    let i = 0;
+    let j = 0;
+    while (i < text.length && j < pattern.length) {
+      if (text[i] === pattern[j]) {
+        i++;
+        j++;
+      }  else {
+        j = lps[j] + 1;
+      }
+    }
+
+    if (j === pattern.length) 
+      return i - j;
+
     return -1;
   }
 
-  buildlps(txt) {
-    if (!txt)
-      return [];
-
+  /*
+   * this one is difficult
+   */
+  buildLPS(txt) {
+    let len = 0;
     const lps = [0];
-    for (let i = 1; i < txt.length; i++) {
-      let j = 0;
-      while (j < i) {
-        if (txt[j] !== txt[i - j])
-          break;
-        j++;
+    let i = 1;
+    while ( i < txt.length) {
+      if (txt[i] === txt[len]) {
+        len++;
+        lps.push(len);
+        i++;
+      } else if (len != 0) {
+          len = lps[len - 1];
+      } else {
+          lps.push(0);
+          i++;
       }
-      lps.push(j);
     }
 
     return lps;
@@ -27,22 +48,22 @@ class Solution {
     let txt = 'AAAAABAAABA';
     let pat = 'AAAA';
 
-    let lps = this.buildlps(pat);
+    let lps = this.buildLPS(pat);
     console.log(`lps of ${pat} is ${lps}`);
 
     // [0, 1, 0, 1, 2, 0, 1, 2, 3, 4, 5]
     pat = "AABAACAABAA";
-    lps = this.buildlps(pat);
+    lps = this.buildLPS(pat);
     console.log(`lps of ${pat} is ${lps}`);
 
     // [0, 1, 2, 0, 1, 2, 3, 3, 3, 4]
     pat = "AAACAAAAAC";
-    lps = this.buildlps(pat);
+    lps = this.buildLPS(pat);
     console.log(`lps of ${pat} is ${lps}`);
 
     // [0, 1, 2, 0, 1, 2, 3]
     pat = "AAABAAA";
-    lps = this.buildlps(pat);
+    lps = this.buildLPS(pat);
     console.log(`lps of ${pat} is ${lps}`);
 
     const result = this.kmp(txt, pat);
